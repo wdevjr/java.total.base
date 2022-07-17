e202b566e9bf443b207a31ca157c81d6a976c032
warhjr <warhjr@gmail.com>
2022-07-17 13:49:01 -0400

    AppAplicationJava Versão 2.0 novo

---
+++ AppAplicationJava Versão 2.0 novo/AppAplicationJava/src/br/com/warhjr/model/Endereco.java
@@ -0,0 +1,170 @@
+package br.com.warhjr.model;
+
+public class Endereco extends Cidade {
+
+	private Integer id_endereco;
+	private Integer cod_cidade;
+	private String logradouro;
+	private String endereco;
+	private Integer numero;
+	private String complemento;
+
+	public Integer getCod_cidade() {
+		return cod_cidade;
+	}
+
+	public void setCod_cidade(Integer cod_cidade) {
+		this.cod_cidade = cod_cidade;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	private String bairro;
+	private String cep;
+	private String email;
+
+	public Integer getId_endereco() {
+		return id_endereco;
+	}
+
+	public void setId_endereco(Integer id_endereco) {
+		this.id_endereco = id_endereco;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bairro == null) ? 0 : bairro.hashCode());
+		result = prime * result + ((cep == null) ? 0 : cep.hashCode());
+		result = prime * result + ((cod_cidade == null) ? 0 : cod_cidade.hashCode());
+		result = prime * result + ((complemento == null) ? 0 : complemento.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+		result = prime * result + ((id_endereco == null) ? 0 : id_endereco.hashCode());
+		result = prime * result + ((logradouro == null) ? 0 : logradouro.hashCode());
+		result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		if (bairro == null) {
+			if (other.bairro != null)
+				return false;
+		} else if (!bairro.equals(other.bairro))
+			return false;
+		if (cep == null) {
+			if (other.cep != null)
+				return false;
+		} else if (!cep.equals(other.cep))
+			return false;
+		if (cod_cidade == null) {
+			if (other.cod_cidade != null)
+				return false;
+		} else if (!cod_cidade.equals(other.cod_cidade))
+			return false;
+		if (complemento == null) {
+			if (other.complemento != null)
+				return false;
+		} else if (!complemento.equals(other.complemento))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (endereco == null) {
+			if (other.endereco != null)
+				return false;
+		} else if (!endereco.equals(other.endereco))
+			return false;
+		if (id_endereco == null) {
+			if (other.id_endereco != null)
+				return false;
+		} else if (!id_endereco.equals(other.id_endereco))
+			return false;
+		if (logradouro == null) {
+			if (other.logradouro != null)
+				return false;
+		} else if (!logradouro.equals(other.logradouro))
+			return false;
+		if (numero == null) {
+			if (other.numero != null)
+				return false;
+		} else if (!numero.equals(other.numero))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Endereco [id_endereco=" + id_endereco + ", cod_cidade=" + cod_cidade + ", logradouro=" + logradouro
+				+ ", endereco=" + endereco + ", numero=" + numero + ", complemento=" + complemento + ", bairro="
+				+ bairro + ", cep=" + cep + ", email=" + email + "]";
+	}
+	
+	
+
+}
